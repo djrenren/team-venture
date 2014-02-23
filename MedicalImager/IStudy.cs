6a2500eb82efe804a177bbe782a181e64e08a079
djrenren <john@jrenner.net>
2014-02-23 12:39:25 -0500

    Implement Study Interface

--- MedicalImager/IStudy.cs
+++ MedicalImager/IStudy.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace MedicalImager
 {
-    class Study : List<BitmapImage>, IStudy
+    interface IStudy
     {
+        BitmapImage this[int index]
+        {
+            get;
+            set;
+        }
     }
 }
