38c40c7fc05eaa65c8f223e7e0267136dbdfc97d
djrenren <john@jrenner.net>
2014-02-23 12:14:17 -0500

    Basic Image Display

---
+++ MedicalImager/SinglePageIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace MedicalImager
+{
+    class SinglePageIterator : StudyIterator
+    {
+        private Study study;
+        public ObservableCollection<BitmapImage> Current {get; set;}
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+        public SinglePageIterator(Study s) 
+        {
+            study = s;
+            Current = new ObservableCollection<BitmapImage>();
+            Current.Insert(0, new BitmapImage(new Uri("C:/Users/John/Desktop/picture-show-flickr-promo.jpg")));
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            return true;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ MedicalImager/StudyIterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MedicalImager
+{
+    interface StudyIterator : IEnumerator<ObservableCollection<BitmapImage>>
+    {
+
+    }
+}
