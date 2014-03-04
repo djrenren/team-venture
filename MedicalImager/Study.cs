968c0d69682bad5c4195d25dd73559adf374084e
wth4280 <wth4280@rit.edu>
2014-03-03 21:59:16 -0500

    Added logic to make study a list of Bitmap Image

--- MedicalImager/Study.cs
+++ MedicalImager/Study.cs
@@ -6,10 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.IO;
+using System.Drawing;
 
 namespace MedicalImager
 {
     class Study : List<BitmapImage>, IStudy
     {
+        public string directory;
+        string[] filePaths;
+
+        public Study(string dir)
+        {
+            filePaths = Directory.GetFiles(dir);
+            directory = dir;
+            foreach (string path in filePaths)
+            {
+                Uri uri = new Uri(path);
+                BitmapImage bm = new BitmapImage (uri);
+                base.Add(bm);
+            }
+        }
     }
 }
