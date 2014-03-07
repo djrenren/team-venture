29df65c8424af5040efe8f51474b1b53876492f1
Joe Ksiazek <hyzer.jak@gmail.com>
2014-03-06 19:24:10 -0500

    Add StudyIteratorSerializer

---
+++ MedicalImager/StudyIteratorSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalImager
+{
+    class StudyIteratorFactory
+    {
+
+
+        public static StudyIterator Create(IStudy study)
+        {
+            string meta = study.GetMeta();
+            string[] lines = meta.Split('\n');
+            string repr = lines[0];
+            int pos = int.Parse(lines[1]);
+
+            if (repr == SingleImageLayout.Representation)
+            {
+                return new SingleImageLayout(study, pos);
+            }
+            else if (repr == TwoByTwoImageLayout.Representation)
+            {
+                return new TwoByTwoImageLayout(study, pos);
+            }
+            return null;
+        }
+    }
+}
