16bcdcba120f121814557a492db84f5dbb9d66e2
Brett Tuttle <tuttle.brett@gmail.com>
2014-04-08 23:10:59 -0400

    added the ability to switch layouts

--- MedicalImager/Commands/SetLayoutCom.cs
+++ MedicalImager/Commands/SetLayoutCom.cs
@@ -9,13 +9,33 @@ namespace MedicalImager.Commands
     class SetLayoutCom : Command
     {
 
+        Type newLayoutType;
+
         public SetLayoutCom(StudyIterator layout, Type type) : base(layout)
         {
+            newLayoutType = type;
         }
 
         public override void Execute()
         {
-
+            if(newLayoutType == typeof(SingleImageLayout))
+            {
+                SingleImageLayout newLayout = new SingleImageLayout(invoker.Study, invoker.Study.Layout.Position);
+                invoker.Study.Layout = newLayout;
+                invoker.Navigate(newLayout);
+            } 
+            else if (newLayoutType == typeof(TwoByTwoImageLayout))
+            {
+                TwoByTwoImageLayout newLayout = new TwoByTwoImageLayout(invoker.Study, invoker.Study.Layout.Position);
+                invoker.Study.Layout = newLayout;
+                invoker.Navigate(newLayout);
+            }
+            else if (newLayoutType == typeof(CoronalReconstruction))
+            {
+                CoronalReconstruction newLayout = new CoronalReconstruction(invoker.Study, invoker.Study.Layout.Position);
+                invoker.Study.Layout = newLayout;
+                invoker.Navigate(newLayout);
+            }
         }
 
         public override void UnExecute()
